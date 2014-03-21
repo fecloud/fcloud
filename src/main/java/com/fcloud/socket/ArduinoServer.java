b0855cf8f1c70733f7fc0e96ac2b8cc581bde5b6
oyf_feng <oyf_feng@163.com>
2014-03-21 09:21:57 +0800

    add test

--- src/main/java/com/fcloud/socket/ArduinoServer.java
+++ src/main/java/com/fcloud/socket/ArduinoServer.java
@@ -30,8 +30,8 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 	private int port;
 
 	private List<ArduinoSocketWorker> workers;
-	
-	private boolean isConnnetd;
+
+	private volatile boolean isConnnetd;
 
 	/**
 	 * @param port
@@ -40,6 +40,21 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 		super();
 		this.port = port;
 		workers = new LinkedList<ArduinoServer.ArduinoSocketWorker>();
+	}
+
+	/**
+	 * @return the isConnnetd
+	 */
+	public boolean isConnnetd() {
+		return isConnnetd;
+	}
+
+	/**
+	 * @param isConnnetd
+	 *            the isConnnetd to set
+	 */
+	public void setConnnetd(boolean isConnnetd) {
+		this.isConnnetd = isConnnetd;
 	}
 
 	/*
@@ -63,7 +78,7 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 					client = socket.accept();
 					socketWorker = new ArduinoSocketWorker(client);
 					workers.add(socketWorker);
-					//new Thread(socketWorker).start();
+					// new Thread(socketWorker).start();
 					socketWorker.run();
 				}
 			}
@@ -102,7 +117,7 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 	@Override
 	public void onMessage(String message) {
 		logger.debug("onMessage message:" + message);
-		if(message.contains("hand")){
+		if (message.contains("hand")) {
 			sendln("ok");
 		}
 	}
@@ -160,6 +175,7 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 		public void run() {
 
 			if (null != socket) {
+				isConnnetd = true;
 				onOpen();
 			}
 
@@ -172,6 +188,7 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 				}
 
 			} catch (IOException e) {
+				setConnnetd(false);
 				onError(e);
 			} finally {
 				onClose();
@@ -187,12 +204,13 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 					out = socket.getOutputStream();
 					out.write(messsage.getBytes("UTF-8"));
 				} catch (IOException e) {
+					setConnnetd(false);
 					onError(e);
 					workers.remove(this);
 					socket = null;
 					onClose();
 				}
-				
+
 			}
 			return false;
 		}
@@ -202,6 +220,7 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 				try {
 					socket.getOutputStream().flush();
 				} catch (IOException e) {
+					setConnnetd(false);
 					onError(e);
 					workers.remove(this);
 					socket = null;
@@ -217,16 +236,20 @@ public class ArduinoServer extends Thread implements ArduinoSocket {
 	}
 
 	public static void main(String[] args) {
+		int port = 80;
+		if(args.length >0){
+			port = Integer.valueOf(args[0]);
+		}
 		ArduinoServer arduinoServer = new ArduinoServer(80);
 		arduinoServer.start();
 		Scanner scanner = new Scanner(System.in);
-		while(true){
+		while (true) {
 			System.out.println("input:");
 			String s = scanner.nextLine();
-			if(arduinoServer.isConnnetd){
+			if (arduinoServer.isConnnetd) {
 				arduinoServer.sendln(s);
 				arduinoServer.flush();
-			}else {
+			} else {
 				System.err.println("not conneted!");
 			}
 		}
